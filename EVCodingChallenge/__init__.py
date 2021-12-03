fd5aebb1cb0c854c3f8c3351f6b0b748c1d03162
Gerry Yang <gerry04y@gmail.com>
2021-12-03 21:18:34 +0000

    Standardize Json

--- EVCodingChallenge/__init__.py
+++ EVCodingChallenge/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from flask import Flask
 from EVCodingChallenge import endpoints
 from cryptography.fernet import Fernet
@@ -14,6 +14,25 @@ def create_app():
         with open("key.key", "w") as key_file:
             encryption_key = Fernet.generate_key()
             key_file.write(encryption_key.decode())
+
+    # json_data = {
+    #     'what': 'the',
+    #     'hell': {
+    #         'is': 'this',
+    #         'huh': '?'
+    #     }
+    # }
+    # print(json_data)
+    # for key in json_data:
+    #     if type(json_data[key]) == dict:
+    #         print(json_data[key])
+
+    # string_json = "{'what': 'the', 'hell': {'is': 'this', 'huh': '?'}}"
+    # string_json = string_json.replace("'", '"')
+    # json_string = json.loads(string_json)
+    # print(json_string)
+    # print(json_string['hell'])
+
     
     app.register_blueprint(endpoints.bp)
 
--- EVCodingChallenge/endpoints.py
+++ EVCodingChallenge/endpoints.py
@@ -1,22 +1,30 @@
 from flask import Blueprint, request, jsonify, current_app
 from cryptography.fernet import Fernet
+import json
 
 bp = Blueprint('endpoints', __name__, url_prefix="/")
+fernet = Fernet('GAVHRmNp3uqui4I0bQ7zP8S2FJrO6e9ZRB1BKwA05vM=')
 
 @bp.route('encrypt', methods=('POST',))
 def encrypt():
     data = request.get_json()
-    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+    # fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
 
     for key in data:
-        data[key] = fernet.encrypt(str(data[key]).encode()).decode()
+        if type(data[key]) == dict: 
+            print(data[key])
+            data[key] = standardize_json(data[key])
+            print(data[key])
+            data[key] = fernet.encrypt(str(data[key]).encode()).decode()
+        else:
+            data[key] = fernet.encrypt(str(data[key]).encode()).decode()
 
     return jsonify(data), 200
 
 @bp.route('decrypt', methods=('POST',))
 def decrypt():
     data = request.get_json()
-    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+    # fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
 
     for key in data:
         if data[key].startswith("gAAAAABhq") == True:
@@ -57,3 +65,31 @@ def verify():
         return 204
     
     return jsonify(data), 400
+
+def standardize_json(data: dict) -> str:
+    sorted_dict = {}
+    
+    for i in sorted(data):
+        sorted_dict[i] = data[i]
+
+    string = str(sorted_dict)
+    string = string.replace("'", '"')
+    string = string.replace(" ", "")
+
+    return string
+
+def convert_string_to_json(string: str) -> dict:
+    string = string.replace("'", '"')
+    json_data = json.loads(string)
+    return json_data
+
+def decrypt_values(data: dict, indicator: str) -> dict:
+    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+
+    for key in data:
+        if data[key].startswith(indicator) == True:
+            data[key] = fernet.decrypt(str(data[key]).encode()).decode()
+    
+    return data
+
+# curl localhost:5000/encrypt -d '{"string data": {"barfoo": "money", "foobar": "honey"}, "boolean data": "True", "integer data": "178"}' -H 'Content-Type: application/json'
