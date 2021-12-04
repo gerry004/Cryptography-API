7ec5a5f68e75d414ffae4d28eed71d2086b31b7d
Gerry Yang <gerry04y@gmail.com>
2021-12-04 14:14:28 +0000

    Helper Functions Hash Signature Standardize Json

--- EVCodingChallenge/endpoints.py
+++ EVCodingChallenge/endpoints.py
@@ -1,70 +1,66 @@
 from flask import Blueprint, request, jsonify, current_app
 from cryptography.fernet import Fernet
 import json
+from hashlib import sha256
 
 bp = Blueprint('endpoints', __name__, url_prefix="/")
-fernet = Fernet('GAVHRmNp3uqui4I0bQ7zP8S2FJrO6e9ZRB1BKwA05vM=')
 
 @bp.route('encrypt', methods=('POST',))
 def encrypt():
     data = request.get_json()
-    # fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
 
     for key in data:
-        if type(data[key]) == dict: 
-            print(data[key])
-            data[key] = standardize_json(data[key])
-            print(data[key])
-            data[key] = fernet.encrypt(str(data[key]).encode()).decode()
-        else:
-            data[key] = fernet.encrypt(str(data[key]).encode()).decode()
+        data[key] = fernet.encrypt(str(data[key]).encode()).decode()
 
     return jsonify(data), 200
 
 @bp.route('decrypt', methods=('POST',))
 def decrypt():
     data = request.get_json()
-    # fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
-
-    for key in data:
-        if data[key].startswith("gAAAAABhq") == True:
-            data[key] = fernet.decrypt(str(data[key]).encode()).decode()
+    decrypted_data = decrypt_json(data, "gAAAAABhq")
     
-    return jsonify(data), 200
+    return jsonify(decrypted_data), 200
 
 @bp.route('sign', methods=('POST',))
 def sign():
-    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
     data = request.get_json()
     signed_data = {}
+    signed_data['data'] = decrypt_json(data, "gAAAAABhq")
+    standardized_json = standardize_json(data)
 
-    for key in data['data']:
-        if data['data'][key].startswith("gAAAAABhq") == True:
-            data['data'][key] = fernet.decrypt(str(data['data'][key]).encode()).decode()
-
-    signed_data['data'] = data
-    signed_data["signature"] = fernet.encrypt(str(data).encode()).decode()
+    print(standardized_json)
+    signed_data["signature"] = compute_signature(standardized_json)
 
     return jsonify(signed_data), 200
 
 @bp.route('verify', methods=('POST',))
 def verify():
-    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
     data = request.get_json()
 
-    for key in data['data']:
-        if data['data'][key].startswith("gAAAAABhq") == True:
-            data['data'][key] = fernet.decrypt(str(data['data'][key]).encode()).decode()
-    
-    computed_signature = fernet.encrypt(str(data['data']).encode()).decode()
+    decrypted_data = decrypt_json(data['data'], "gAAAAABhq")
+    standardized_json = standardize_json(decrypted_data)
 
-    if computed_signature == data['signature']:
-        print(data['signature'], 'initial signature')
-        print(computed_signature, 'computed signature')
-        print('verified')
-        return 204
+    print(standardized_json)
+    signature = compute_signature(standardized_json)
+
+    if signature == data['signature']:
+        return jsonify(), 204
     
-    return jsonify(data), 400
+    return jsonify(initial=data['signature'], computed=signature), 400
+
+def decrypt_json(data: dict, indicator: str) -> dict:
+    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
+
+    for key in data:
+        if str(data[key]).startswith(indicator) == True:
+            data[key] = fernet.decrypt(str(data[key]).encode()).decode()
+            try:
+                data[key] = convert_string_to_json(data[key])
+            except Exception as e:
+                print(e)
+    
+    return data
 
 def standardize_json(data: dict) -> str:
     sorted_dict = {}
@@ -78,18 +74,12 @@ def standardize_json(data: dict) -> str:
 
     return string
 
+def compute_signature(data: str) -> str: 
+    signature = sha256(data.encode()).hexdigest()
+
+    return signature
+
 def convert_string_to_json(string: str) -> dict:
     string = string.replace("'", '"')
     json_data = json.loads(string)
     return json_data
-
-def decrypt_values(data: dict, indicator: str) -> dict:
-    fernet = Fernet(current_app.config['ENCRYPTION_KEY'])
-
-    for key in data:
-        if data[key].startswith(indicator) == True:
-            data[key] = fernet.decrypt(str(data[key]).encode()).decode()
-    
-    return data
-
-# curl localhost:5000/encrypt -d '{"string data": {"barfoo": "money", "foobar": "honey"}, "boolean data": "True", "integer data": "178"}' -H 'Content-Type: application/json'
